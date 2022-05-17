29551f35becd86d7e72706e9552dd5ad490a324b
Samuel Charles <samyrd309@gmail.com>
2022-05-17 05:01:14 -0400

    7ma

--- decimaltoRoman.py
+++ decimaltoRoman.py
@@ -1,14 +1,20 @@
 class DecimalRoman(object):
+  
   def decimal_to_roman(value):
+    
     while value != None:
-      romanValue = [1000000, 900000, 500000, 400000, 100000, 90000, 50000, 40000, 10000, 9000, 5000, 4000, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-      romanSymbols = ['M`', 'C`M`', 'D`', 'C`D`', 'C`', 'X`C`', 'L`', 'X`L`','X`','I`X`','V`','I`V`','M','CM','D','CD','C','XC','L', 'XL', 'X', 'IX','V','IV','I' ]
-      i = 0
-      romanOutput = ''
-      for i in range(len(romanValue)):
-        while value >= romanValue[i]:
-          romanOutput += romanSymbols[i]
-          value -=  romanValue[i]
+      if value >= 2000000 or value < 0:
+        romanOutput = 'Error'
+        return romanOutput
+      else:
+        romanValue = [1000000, 900000, 500000, 400000, 100000, 90000, 50000, 40000, 10000, 9000, 5000, 4000, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        romanSymbols = ['M`', 'C`M`', 'D`', 'C`D`', 'C`', 'X`C`', 'L`', 'X`L`','X`','I`X`','V`','I`V`','M','CM','D','CD','C','XC','L', 'XL', 'X', 'IX','V','IV','I' ]
+        i = 0
+        romanOutput = ''
+        for i in range(len(romanValue)):
+          while value >= romanValue[i]:
+            romanOutput += romanSymbols[i]
+            value -=  romanValue[i]
       return romanOutput
     
   def validate():
@@ -16,10 +22,9 @@ class DecimalRoman(object):
       romanInput = input('Ingresar valor: ')
       try:
         romanInput = int(romanInput)
-        if romanInput < 2000000 and romanInput > 0:
-          return romanInput
-        else:
-          romanInput ='Valor fuera del rango'
+        if romanInput > 1999999 or romanInput < 0:
+          romanInput = 'Valor ingresado invalido'
+          print(romanInput)
           return romanInput
       except ValueError:
         print('Valor ingresado invalido')
--- testValidate.py
+++ testValidate.py
@@ -2,8 +2,11 @@ import unittest
 from decimaltoRoman import DecimalRoman 
     
 class TestValidateRoman(unittest.TestCase):
-  def test_value_1(self):
+  def test_value_out_of_range(self):
       self.assertEqual('Valor fuera del rango', DecimalRoman.validate())
+      
+  def text_value_null(self):
+    self.assertEqual('Valor ingresado invalido', DecimalRoman.validate())
 
 if __name__ == '__main__': 
   unittest.main()
